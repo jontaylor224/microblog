31fcae3e53cb4007d21d959b4b425db325d12b4a
Jon Taylor <jontaylor224@gmail.com>
2019-07-21 19:33:46 -0500

    pep8 cleanup

--- app/models.py
+++ app/models.py
@@ -12,6 +12,7 @@ class User(db.Model):
     def __repr__(self):
         return '<User: {}>'.format(self.username)
 
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
